4270252ae10293dc3ffbb0b375f3cf7b7fda0424
Geoffrey Borough <105998328+gborough@users.noreply.github.com>
2024-01-29 08:50:38 +1100

    add ocamlformat (#3)

--- lib/quotient.mli
+++ lib/quotient.mli
@@ -1,60 +1,57 @@
 (** Quotient Representation
 
-    Currency operates on rational types
-*)
+    Currency operates on rational types *)
 
-module Make (Qv: Qv_intf.S) : sig
+module Make (Qv : Qv_intf.S) : sig
   exception CurrencyTypeMismatch of string
 
   (** {1 Types} *)
-  type t = {
-    symbol: string; (** Currency Symbol *)
-    value: Qv.t; (** Value in Rational *)
-  }
+  type t =
+    { symbol : string (** Currency Symbol *)
+    ; value : Qv.t (** Value in Rational *)
+    }
   [@@deriving compare]
 
-  type showable = {
-    symbol_: string; [@key "symbol"]
-    value_: string; [@key "value"]
-  }
-  [@@deriving show, sexp, yojson]
   (** showable for t *)
+  type showable =
+    { symbol_ : string [@key "symbol"]
+    ; value_ : string [@key "value"]
+    }
+  [@@deriving show, sexp, yojson]
 
   (** {1 Construction} *)
   val make_qv : string * Qv.t -> t
   (** Construct quotient value, e.g. make_qv "GBP" Utils.make_q("123456/100") *)
 
-  val show_sym : t -> unit
   (** Print the currency symbol *)
+  val show_sym : t -> unit
 
-  val show_val : t -> unit
   (** Print the currency value *)
+  val show_val : t -> unit
 
-  val show_t : t -> unit
   (** Print the quotient value *)
+  val show_t : t -> unit
 
-  val neg : t -> t
   (** Negate a quotient value *)
+  val neg : t -> t
 
-  val abs : t -> t
   (** Return an absolute quotient value *)
+  val abs : t -> t
 
-  val ( + ): t -> t -> t
   (** Add two quotient values with same symbol as required *)
+  val ( + ) : t -> t -> t
 
-  val ( - ): t -> t -> t
   (** Substract two quotient values with same symbol as required *)
+  val ( - ) : t -> t -> t
 
-  val ( * ): t: t -> value: Qv.t -> t
   (** Multiply a quotient value with a plain rational value.
-      Multiplying two quotient values does not make sense for currency ops.
-  *)
+      Multiplying two quotient values does not make sense for currency ops. *)
+  val ( * ) : t:t -> value:Qv.t -> t
 
-  val ( / ): t -> Qv.t -> t
   (** Divide a quotient value with a plain rational value.
-      Dividing two quotient values does not make sense for currency ops.
-  *)
+      Dividing two quotient values does not make sense for currency ops. *)
+  val ( / ) : t -> Qv.t -> t
 
-  val to_showable_json : t -> string
   (** Convert t to showable rep *)
+  val to_showable_json : t -> string
 end
--- lib/zv_intf.ml
+++ lib/zv_intf.ml
@@ -1,55 +1,50 @@
 (** Integer Representation
 
-    Wrapper type for Zarith Z integer type {{:https://ocaml.org/p/zarith/latest/doc/Z/index.html}[Z]}
+    Wrapper type for Zarith Z integer type {{:https://ocaml.org/p/zarith/latest/doc/Z/index.html} [Z]}
     which serves as the underlying type for other currency types that are built based on integer values.
 
     Apart from few helper conversion functions, this signature implement most of what is equivalent to
-    the Num typeclass sensibly.
-*)
+    the Num typeclass sensibly. *)
 
 module type S = sig
   (** {1 Types} *)
   type t = private Z.t [@@deriving compare]
 
-  module S: sig
+  module S : sig
     (** {1 Construction} *)
     val make : string -> t
     (** Constructing Z by using a private type, create a Z value from string, e.g. [from_str "123"].
-    Reason behind is to disallow a range of functions that are unsuitable for currency operations.
-    For convenience the function alias [make_z] can be used.
-    *)
+        Reason behind is to disallow a range of functions that are unsuitable for currency operations.
+        For convenience the function alias [make_z] can be used. *)
 
-    type showable = {
-      value: string
-    }
-    [@@deriving show, sexp, yojson]
     (** showable for t *)
+    type showable = { value : string } [@@deriving show, sexp, yojson]
 
-    val neg : t -> t
     (** Negate a Z value *)
+    val neg : t -> t
 
-    val abs : t -> t
     (** Return an absolute Z value *)
+    val abs : t -> t
 
-    val signum : t -> int
     (** Return the sign of Z value, -1, 0, or 1 when the argument is respectively negative, null, or positive. *)
+    val signum : t -> int
 
-    val add : t -> t -> t
     (** Add two Z values *)
+    val add : t -> t -> t
 
-    val sub : t -> t -> t
     (** Substract two Z values *)
+    val sub : t -> t -> t
 
-    val mul : t -> t -> t
     (** Multiply two Z values *)
+    val mul : t -> t -> t
 
-    val to_str : t -> string
     (** Convert a Z value to string *)
+    val to_str : t -> string
 
-    val to_float : t -> float
     (** Convert a Z value to float *)
+    val to_float : t -> float
 
-    val to_showable_json : t -> string
     (** Convert a Q value to printable rep *)
+    val to_showable_json : t -> string
   end
 end
